e99638866f4dca22b636cba73c3432f1a7796960
Vitaly <faetonvs@gmail.com>
2020-01-20 16:23:10 +0500

    Bearer auth setup next

---
+++ config/initializers/bearer.rb
@@ -0,0 +1,11 @@
+=begin
+Bearer::Configuration.setup do |config|
+  config.api_key = "BEARER_SECRET_KEY" # copy and paste the `Secret Key` from https://app.bearer.sh/keys
+  config.client_id = "BEARER_PUBLISHABLE_KEY" # copy and paste the `Publishable Key` from https://app.bearer.sh/keys
+  config.encryption_key = "secret" # copy and paste the `Encryption Key` from https://app.bearer.sh/keys
+end
+=end
+
+Bearer.init_config do |config|
+  config.secret_key = "sk_production_PTbWALhx_4O7syBTTw9oMuvFoTWzpggB"
+end
