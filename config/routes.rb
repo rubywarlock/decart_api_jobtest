33486b37006e5d55ea0522538d56e6016c81ed06
Vitaly <faetonvs@gmail.com>
2020-01-21 11:32:29 +0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  mount BearerRails::Webhooks.new, at: '/currencies'
-  mount BearerRails::Webhooks.new, at: '/currency/:id'
-  mount BearerRails::Webhooks.new, at: '/'
-
-  #get "currencies",   to: "currencies#index"
-  #get "currency/:id", to: "currencies#show"
+  resources :currencies, only: [:index, :show]
 
   root "currencies#index"
 end
