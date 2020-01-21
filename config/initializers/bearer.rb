83ac6b9c557c7281d3552c56167233dc5fff840b
Vitaly <faetonvs@gmail.com>
2020-01-21 11:34:22 +0500

    routes

--- config/initializers/bearer.rb
+++ config/initializers/bearer.rb
@@ -1,19 +1,3 @@
-=begin
 Bearer::Configuration.setup do |config|
-  config.api_key = "BEARER_SECRET_KEY" # copy and paste the `Secret Key` from https://app.bearer.sh/keys
-  config.client_id = "BEARER_PUBLISHABLE_KEY" # copy and paste the `Publishable Key` from https://app.bearer.sh/keys
-  # config.encryption_key = "secret" # copy and paste the `Encryption Key` from https://app.bearer.sh/keys
-  config.secret_key = "sk_production_PTbWALhx_4O7syBTTw9oMuvFoTWzpggB" # copy and paste the `Encryption Key` from https://app.bearer.sh/keys
+  config.secret = "sk_production_PTbWALhx_4O7syBTTw9oMuvFoTWzpggB"
 end
-=end
-
-Bearer::Configuration.setup do |config|
-  # config.api_key = "sk_production_PTbWALhx_4O7syBTTw9oMuvFoTWzpggB"
-  config.secret = "sk_production_PTbWALhx_4O7syBTTw9oMuvFoTWzpggB" ## Used for the WebHook
-end
-
-=begin
-Bearer.init_config do |config|
-  config.secret_key = "sk_production_PTbWALhx_4O7syBTTw9oMuvFoTWzpggB"
-end
-=end
